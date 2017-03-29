3d2ff3f9df30d787745cdaadc7b91bb223573638
lalocahuich <lalo.cahuich@gmail.com>
2017-03-28 20:04:21 -0600

    Cambios de Imagenes

--- CoflexWeb/Default.aspx.vb
+++ CoflexWeb/Default.aspx.vb
@@ -6,7 +6,7 @@ Public Class _Default
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
 
 
-        Me.response.InnerText = CoflexWebServices.doPostRequest(CoflexWebServices.LOGIN, "grant_type=password&username=luis.ibarra0992@gmail.com&password=a")
+        'Me.response.InnerText = CoflexWebServices.doPostRequest(CoflexWebServices.LOGIN, "grant_type=password&username=luis.ibarra0992@gmail.com&password=a")
 
 
     End Sub
