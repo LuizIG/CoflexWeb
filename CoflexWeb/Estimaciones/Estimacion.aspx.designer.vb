1889d8904f7db1b89c3ffa26333b30a89ef67798
Luis Ibarra <luis.ibarra0992@gmail.com>
2017-06-10 21:40:32 -0500

    closes #12 se cambia boton versionar por guardar nueva version

--- CoflexWeb/Estimaciones/Estimacion.aspx.designer.vb
+++ CoflexWeb/Estimaciones/Estimacion.aspx.designer.vb
@@ -689,13 +689,13 @@ Partial Public Class Estimacion
     Protected WithEvents Guardar As Global.System.Web.UI.WebControls.Button
     
     '''<summary>
-    '''Control Guardar Nueva Version.
+    '''Control Versionar.
     '''</summary>
     '''<remarks>
     '''Campo generado automáticamente.
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
-    Protected WithEvents Guardar Nueva Version As Global.System.Web.UI.WebControls.Button
+    Protected WithEvents Versionar As Global.System.Web.UI.WebControls.Button
     
     '''<summary>
     '''Control View3.
